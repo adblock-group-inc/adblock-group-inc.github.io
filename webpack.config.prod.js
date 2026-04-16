d90188392a52d3c5c5eba09608b25c374f1f600a
malteww <maltewallander1@gmail.com>
2026-04-16 19:23:54 +0200

    update webpack config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -6,18 +6,28 @@ const CopyPlugin = require('copy-webpack-plugin');
 module.exports = merge(common, {
   mode: 'production',
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-    }),
+    new HtmlWebpackPlugin({ template: './index.html', filename: 'index.html' }),
+    new HtmlWebpackPlugin({ template: './products.html', filename: 'products.html', inject: false }),
+    new HtmlWebpackPlugin({ template: './contact.html', filename: 'contact.html', inject: false }),
+    new HtmlWebpackPlugin({ template: './checkout.html', filename: 'checkout.html', inject: false }),
     new CopyPlugin({
       patterns: [
         { from: 'public', to: 'public' },
         { from: 'css', to: 'css' },
         { from: 'js/vendor', to: 'js/vendor' },
-        { from: 'icon.svg', to: 'icon.svg' },
         { from: 'favicon.ico', to: 'favicon.ico' },
+        { from: 'favicon.svg', to: 'favicon.svg' },
+        { from: 'favicon-16x16.png', to: 'favicon-16x16.png' },
+        { from: 'favicon-32x32.png', to: 'favicon-32x32.png' },
+        { from: 'favicon-48x48.png', to: 'favicon-48x48.png' },
+        { from: 'android-chrome-192x192.png', to: 'android-chrome-192x192.png' },
+        { from: 'android-chrome-512x512.png', to: 'android-chrome-512x512.png' },
+        { from: 'apple-touch-icon.png', to: 'apple-touch-icon.png' },
+        { from: 'apple-touch-icon-152x152.png', to: 'apple-touch-icon-152x152.png' },
+        { from: 'apple-touch-icon-167x167.png', to: 'apple-touch-icon-167x167.png' },
+        { from: 'apple-touch-icon-180x180.png', to: 'apple-touch-icon-180x180.png' },
+        { from: 'safari-pinned-tab.svg', to: 'safari-pinned-tab.svg' },
         { from: 'robots.txt', to: 'robots.txt' },
-        { from: 'icon.png', to: 'icon.png' },
         { from: '404.html', to: '404.html' },
         { from: 'site.webmanifest', to: 'site.webmanifest' },
       ],
